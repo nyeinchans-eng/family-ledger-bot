aa172421fbfef0b29906e682d2964d42361f7658
nyeinchans-eng <nyeinchans@gmail.com>
2026-08-19 15:04:56 +0630

    Update migrate.js

--- db/migrate.js
+++ db/migrate.js
@@ -1,10 +1,14 @@
+Migrate · JS
 import "dotenv/config";
 import fs from "fs";
 import pg from "pg";
-
+ 
 const sql = fs.readFileSync(new URL("./schema.sql", import.meta.url), "utf8");
-const client = new pg.Client({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
-
+const client = new pg.Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.PGSSL === "false" ? false : { rejectUnauthorized: false },
+});
+ 
 (async () => {
   await client.connect();
   await client.query(sql);
